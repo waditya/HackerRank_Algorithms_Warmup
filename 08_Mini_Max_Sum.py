a616db81a18b4043f7d9ef66d74463a5a62c447c
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2017-12-27 08:06:24 -0600

    Update 08_Mini_Max_Sum.py

--- 08_Mini_Max_Sum.py
+++ 08_Mini_Max_Sum.py
@@ -3,14 +3,4 @@
 import sys
 
 arr = list(map(int, input().strip().split(' ')))
-arr.sort
-length_of_array = len(arr)
-max_total = 0
-min_total = 0
-
-for i in range(0,length_of_array - 1, 1 ):
-    min_total = min_total + arr[i]
-    
-for j in range(1, length_of_array, 1):
-    max_total = max_total + arr[j]
-print(min_total, max_total )
+print(sum(arr) - max(arr), sum(arr) - min(arr) )
